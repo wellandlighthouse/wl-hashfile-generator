08aeb47f9574ce2bfe80208613eb20585db05d6b
JC Quirin <jc@wellandlighthouse.com>
2017-09-21 10:53:21 -0700

    we now default to transforming to uppercase before performing md5 hash, but service allows for other transformations via options

--- src/app/shared/hasher.service.ts
+++ src/app/shared/hasher.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import * as _ from 'lodash';
-var md5 = require('md5');
+const md5 = require('md5');
 
 @Injectable()
 export class HasherService {
@@ -10,12 +10,25 @@ export class HasherService {
 
 	hashField(data: {}[], fields: Array<string>, options?: {}) {
 		let opts = _.merge({
-			alg: 'md5'
+			alg: 'md5',
+			transform: 'upper',
 		}, options);
 
 		for (let item of data) {
 			for (let field of fields) {
-				item[field] = md5(item[field]);
+				let val: string;
+				switch (opts.transform) {
+					case 'upper':
+						val = item[field].toUpperCase();
+						break;
+					case 'lower':
+						val = item[field].toLowerCase();
+						break;
+					default:
+						val = item[field];
+						break;
+				}
+				item[field] = md5(val);
 			}
 		}
 		return data;
